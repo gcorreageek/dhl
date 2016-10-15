e7c38f12aa2402bb71d008df1c5c1ff33a084b41
Gusta <gcorreageek@gmail.com>
2016-10-15 00:11:32 -0300

    o menu

--- frontend/ProyClient1/app/src/main/java/com/dhl/proyclient1/LoginActivity.java
+++ frontend/ProyClient1/app/src/main/java/com/dhl/proyclient1/LoginActivity.java
@@ -1,30 +1,12 @@
 package com.dhl.proyclient1;
 
-import android.app.Application;
 import android.content.Intent;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -43,38 +25,7 @@ public class LoginActivity extends AppCompatActivity {
         final EditText user1 = (EditText) findViewById(R.id.editText);
         final EditText pass1 = (EditText) findViewById(R.id.editText2);
         final Button botomLogin = (Button) findViewById(R.id.button);
-        /*botomLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final String user2 = user1.getText().toString();
-                final String pass2 = pass1.getText().toString();
-                Log.i("LoginActivity","user2!!"+user2);
-                Log.i("LoginActivity","pass2!!"+pass2);
 
-                Map<String,String> params = new HashMap<>();
-                params.put("username",user2);
-                params.put("password",pass2);
-                params.put("rememberMe","true");
-
-                StringRequest stringRequest = new StringRequest(Request.Method.POST, LOGIN_REQUEST_URL,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                Log.i("LoginActivity","response!!"+response);
-//                                mTextView.setText("Response is: "+ response.substring(0,500));
-                            }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.i("LoginActivity","error!!"+error);
-                        Log.i("LoginActivity","error!!"+error);
-//                        mTextView.setText("That didn't work!");
-                    }
-                });
-                Volley.newRequestQueue(LoginActivity.this).add(stringRequest);
-
-            }
-        });*/
         botomLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -83,36 +34,15 @@ public class LoginActivity extends AppCompatActivity {
                 Log.i("LoginActivity","user2!!"+user2);
                 Log.i("LoginActivity","pass2!!"+pass2);
 
-                Map<String,String> params;
 
-                params = new HashMap<>();
-                params.put("username",user2);
-                params.put("password",pass2);
-                params.put("rememberMe","true");
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                intent.putExtra("login","gcorreageek");
+                intent.putExtra("firstName","Gustavo Antonio");
+                intent.putExtra("lastName","Correa Caja");
+                intent.putExtra("email","gcorreageek@gmail.com");
 
 
-                JsonObjectRequest req = new JsonObjectRequest(LOGIN_REQUEST_URL, new JSONObject(params),
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            Log.i("LoginActivity","response!!"+response);
-                            try {
-                                Log.i("LoginActivity","response!!"+response.get("id_token").toString());
-                                requestWithSomeHttpHeaders(response.get("id_token").toString());
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
-                        builder.setMessage("User o Pass incorrectos").setNegativeButton("Retry",null).create().show();
-
-                        VolleyLog.e("Error1: ", error.getMessage());
-                    }
-                });
-                Volley.newRequestQueue(LoginActivity.this).add(req);
+                LoginActivity.this.startActivity(intent);
 
 
 
@@ -126,65 +56,4 @@ public class LoginActivity extends AppCompatActivity {
 
 
 
-
-    public void requestWithSomeHttpHeaders(final String token) {
-        RequestQueue queue = Volley.newRequestQueue(this);
-        StringRequest postRequest = new StringRequest(Request.Method.GET, ACCOUNT_GET_URL,
-                new Response.Listener<String>()
-                {
-                    @Override
-                    public void onResponse(String response) {
-                        Log.d("Response", response);
-                        try {
-                            JSONObject jsonObject = new JSONObject(response);
-                            String login = jsonObject.getString("login");
-                            Log.i("LoginActivity","login!!"+login);
-                            String firstName = jsonObject.getString("firstName");
-                            Log.i("LoginActivity","firstName!!"+firstName);
-                            String lastName = jsonObject.getString("lastName");
-                            Log.i("LoginActivity","lastName!!"+lastName);
-                            String email = jsonObject.getString("email");
-                            Log.i("LoginActivity","email!!"+email);
-
-
-                            Intent intent = new Intent(LoginActivity.this,WelcomeActivity.class);
-                            intent.putExtra("login",login);
-                            intent.putExtra("firstName",firstName);
-                            intent.putExtra("lastName",lastName);
-                            intent.putExtra("email",email);
-
-
-                            LoginActivity.this.startActivity(intent);
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-
-
-
-
-
-
-                    }
-                },
-                new Response.ErrorListener()
-                {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.d("ERROR","error => "+error.toString());
-                    }
-                }
-        ) {
-            @Override
-            public Map<String, String> getHeaders()  {
-                Map<String, String>  params = new HashMap<String, String>();
-                params.put("Accept", "application/json, text/plain, */*");
-                params.put("Authorization", "Bearer " + token);
-                return params;
-            }
-        };
-        queue.add(postRequest);
-
-    }
 }
---
+++ frontend/ProyClient1/app/src/main/java/com/dhl/proyclient1/MainActivity.java
@@ -0,0 +1,79 @@
+package com.dhl.proyclient1;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Log.i("MainActivity","id!!"+id);
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            Log.i("MainActivity","id!!"+id);
+        } else if (id == R.id.nav_gallery) {
+            Log.i("MainActivity","id!!"+id);
+        } else if (id == R.id.nav_slideshow) {
+            Log.i("MainActivity","id!!"+id);
+        } else if (id == R.id.nav_manage) {
+            Log.i("MainActivity","id!!"+id);
+        } else if (id == R.id.nav_share) {
+            Log.i("MainActivity","id!!"+id);
+        } else if (id == R.id.nav_send) {
+            Log.i("MainActivity","id!!"+id);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
