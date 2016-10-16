8e90046ac7d5dfe9857341e719780559fee5905d
Gusta <gcorreageek@gmail.com>
2016-10-16 05:14:46 -0200

    list view ok

--- frontend/ProyClient1/app/src/main/java/com/dhl/proyclient1/MainActivity.java
+++ frontend/ProyClient1/app/src/main/java/com/dhl/proyclient1/MainActivity.java
@@ -2,13 +2,21 @@ package com.dhl.proyclient1;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -23,12 +31,63 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
 
+
+
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+
+        adapter.addFragment(new ListContentFragment(), null);
+        adapter.addFragment(new TileContentFragment(), null);
+        adapter.addFragment(new CardContentFragment(), null);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+
+
+        tabs.setupWithViewPager(viewPager);
+        tabs.getTabAt(0).setIcon(R.drawable.ic_grain_black_24dp);
+        tabs.getTabAt(1).setIcon(R.drawable.ic_thumbs_up_down_black_24dp);
+        tabs.getTabAt(2).setIcon(R.drawable.ic_favorite_border_black_24dp);
+
+
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
     }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public Adapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
 
     @Override
     public void onBackPressed() {
@@ -44,7 +103,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        Log.i("MainActivity","id!!"+id);
+        Log.i("MainActivity", "id!!" + id);
 //        if (id == R.id.action_settings) {
 //            return true;
 //        }
@@ -59,17 +118,17 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_home) {
-            Log.i("MainActivity","id!!"+id);
+            Log.i("MainActivity", "id!!" + id);
         } else if (id == R.id.nav_edit) {
-            Log.i("MainActivity","id!!"+id);
+            Log.i("MainActivity", "id!!" + id);
         } else if (id == R.id.nav_close) {
-            Log.i("MainActivity","id!!"+id);
+            Log.i("MainActivity", "id!!" + id);
         } else if (id == R.id.nav_config) {
-            Log.i("MainActivity","id!!"+id);
+            Log.i("MainActivity", "id!!" + id);
         } else if (id == R.id.nav_info) {
-            Log.i("MainActivity","id!!"+id);
+            Log.i("MainActivity", "id!!" + id);
         } else if (id == R.id.nav_web) {
-            Log.i("MainActivity","id!!"+id);
+            Log.i("MainActivity", "id!!" + id);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
