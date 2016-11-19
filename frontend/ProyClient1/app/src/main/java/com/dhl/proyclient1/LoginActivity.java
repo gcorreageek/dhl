a76021a0e5ce2c270bc4879c67703390c2ae17d6
Gusta <gcorreageek@gmail.com>
2016-11-19 16:48:27 -0200

    token

--- frontend/ProyClient1/app/src/main/java/com/dhl/proyclient1/LoginActivity.java
+++ frontend/ProyClient1/app/src/main/java/com/dhl/proyclient1/LoginActivity.java
@@ -2,11 +2,27 @@ package com.dhl.proyclient1;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -35,15 +51,40 @@ public class LoginActivity extends AppCompatActivity {
                 Log.i("LoginActivity","pass2!!"+pass2);
 
 
-                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-                intent.putExtra("login","gcorreageek");
-                intent.putExtra("firstName","Gustavo Antonio");
-                intent.putExtra("lastName","Correa Caja");
-                intent.putExtra("email","gcorreageek@gmail.com");
+//                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+//                intent.putExtra("login","gcorreageek");
+//                intent.putExtra("firstName","Gustavo Antonio");
+//                intent.putExtra("lastName","Correa Caja");
+//                intent.putExtra("email","gcorreageek@gmail.com");
+
+                Map<String,String> params = new HashMap<>();
+                params.put("username",user2);
+                params.put("password",pass2);
+                params.put("rememberMe","true");
 
 
-                LoginActivity.this.startActivity(intent);
+                JsonObjectRequest req = new JsonObjectRequest(LOGIN_REQUEST_URL, new JSONObject(params),
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Log.i("LoginActivity","response!!"+response);
+                                try {
+                                    Log.i("LoginActivity","response!!"+response.get("id_token").toString());
+                                    requestWithSomeHttpHeaders(response.get("id_token").toString());
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                        builder.setMessage("User o Pass incorrectos").setNegativeButton("Retry",null).create().show();
 
+                        VolleyLog.e("Error1: ", error.getMessage());
+                    }
+                });
+                Volley.newRequestQueue(LoginActivity.this).add(req);
 
 
 
@@ -51,6 +92,63 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+
+
+
+    public void requestWithSomeHttpHeaders(final String token) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest postRequest = new StringRequest(Request.Method.GET, ACCOUNT_GET_URL,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("Response", response);
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String login = jsonObject.getString("login");
+                            Log.i("LoginActivity","login!!"+login);
+                            String firstName = jsonObject.getString("firstName");
+                            Log.i("LoginActivity","firstName!!"+firstName);
+                            String lastName = jsonObject.getString("lastName");
+                            Log.i("LoginActivity","lastName!!"+lastName);
+                            String email = jsonObject.getString("email");
+                            Log.i("LoginActivity","email!!"+email);
+
+
+                            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                            intent.putExtra("login",login);
+                            intent.putExtra("firstName",firstName);
+                            intent.putExtra("lastName",lastName);
+                            intent.putExtra("email",email);
+
+
+                            LoginActivity.this.startActivity(intent);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("ERROR","error => "+error.toString());
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders()  {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("Accept", "application/json, text/plain, */*");
+                params.put("Authorization", "Bearer " + token);
+                return params;
+            }
+        };
+        queue.add(postRequest);
+
+    }
 
 
 
